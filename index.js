6e91ec714566e81f094ea377d2c447c608d5b83b
VishalMSahani <vishalsahani0808@gmail.com>
2024-06-19 15:33:07 +0530

    encryption & decryption done

---
+++ index.js
@@ -0,0 +1,56 @@
+const crypto = require("crypto")
+
+const PROBLEM_1 = 'what the tempertaure outside';
+const option1 = '23'
+const option2 = '27'
+const option3 = '26'
+const option4 = '29'
+
+
+function encryption(text ,key ,iv){
+    const algo = 'aes-256-cbc'
+
+    const cipher = crypto.createCipheriv(algo,key,iv);
+
+    let  encrypt = cipher.update(text,'utf8', 'hex');
+     encrypt += cipher.final('hex')
+     return encrypt;
+}
+
+function deCryption(encrypt , key, iv){
+    const algo = 'aes-256-cbc';
+
+    const decypher = crypto.createDecipheriv(algo,key,iv);
+    let decrypt = decypher.update(encrypt,'hex','utf8');
+    decrypt += decypher.final('utf8')
+    return decrypt;
+}
+
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+const encryptedProblem = encryption(PROBLEM_1,key,iv);
+const encryptedsol1 = encryption(option1,key,iv);
+const encryptedsol2 = encryption(option2,key,iv);
+const encryptedsol3 = encryption(option3,key,iv);
+const encryptedsol4 = encryption(option4,key,iv);
+
+console.log('encrypted problem statement',encryptedProblem);
+console.log('encrypted problem solution1',encryptedsol1);
+console.log('encrypted problem solution2',encryptedsol2);
+console.log('encrypted problem solution3',encryptedsol3);
+console.log('encrypted problem solution4',encryptedsol4);
+
+
+const decryptedProblem = deCryption(encryptedProblem,key,iv)
+const orignalsol1 = deCryption(encryptedsol1,key,iv)
+const orignalsol2 = deCryption(encryptedsol2,key,iv)
+const orignalsol3 = deCryption(encryptedsol3,key,iv)
+const orignalsol4 = deCryption(encryptedsol4,key,iv)
+
+console.log('decrypted problem statement',decryptedProblem);
+console.log('decrypted problem sol1',orignalsol1);
+console.log('decrypted problem solution2',orignalsol2 );
+console.log('decrypted problem solution3',orignalsol3);
+console.log('decrypted problem solution4',orignalsol4);
+
